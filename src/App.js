bc49e2077e8a839f118b6ba5ba840cccd66cb0a5
Maciej <1994tomaszewicz@gmail.com>
2020-06-28 00:41:37 +0200

    app almost finished

--- src/App.js
+++ src/App.js
@@ -21,6 +21,67 @@ function App() {
   const [number, setNumber] = useState()
   const [colors, setColors] = useState(["red"]);
   const [classState, setClassState] = useState({isSwitchOn: false})
+    const [classPulsate, setClassPulsate] = useState({
+      first: true,
+      second: false,
+      third: false
+
+    })
+  const [textInDiv, setTextInDiv] = useState(true)
+  const [click, setClick] = useState(true)
+  const [checkboxes, setCheckboxes] = useState({
+    first: true,
+    second: false,
+    third: false,
+    fourth: false,
+    fifth: false,
+    sixth: false
+  })
+  ///////////////////////////CHECKBOXES FUNCTIONS
+  const handleFirstCheckbox = () => {
+    setCheckboxes({...checkboxes,
+    first: !checkboxes.first})
+
+  }
+  const handleSecondCheckbox = () => {
+    setCheckboxes({...checkboxes,
+    second: !checkboxes.second})
+
+  }
+  const handleThirdCheckbox = () => {
+    setCheckboxes({...checkboxes,
+    third: !checkboxes.third})
+
+  }
+  const handleFourthCheckbox = () => {
+    setCheckboxes({...checkboxes,
+    fourth: !checkboxes.fourth})
+
+  }
+  const handleFifthCheckbox = () => {
+    setCheckboxes({...checkboxes,
+    fifth: !checkboxes.fifth})
+
+  }
+  const handleSixthCheckbox = () => {
+    setCheckboxes({...checkboxes,
+    sixth: !checkboxes.sixth})
+
+  }
+  const allCheckboxes = () => {
+    if (checkboxes.first == false &&
+    checkboxes.second == false &&
+    checkboxes.third == false &&
+    checkboxes.fourth == false &&
+    checkboxes.fifth == false &&
+    checkboxes.sixth == false) {
+      return false;
+    }else {
+      return true;
+    }
+  }
+  allCheckboxes()
+  console.log(allCheckboxes)
 
   console.log(colors)
   console.log(colors.length)
@@ -77,11 +138,14 @@ const handleWhoGive = event => {
   };
 
   const styles = {
-    red: { backgroundColor: "red" },
-    brown: { backgroundColor: "brown" },
-    blue: { backgroundColor: "blue" },
-    yellow: { backgroundColor: "yellow" },
-    orange: { backgroundColor: "orange" }
+    red: { backgroundColor: "red", color: "white" },
+    brown: { backgroundColor: "saddlebrown", color: "white" },
+    blue: { backgroundColor: "blue", color: "white" },
+    yellow: { backgroundColor: "yellow", color: "white" },
+    orange: { backgroundColor: "orange", color: "white" },
+    purple: {background: "purple", color: "white"}
+
+
   };
 
 function loop() {
@@ -113,6 +177,11 @@ function loop() {
     setArr(array);
     array = [];
 
+
+    setClassPulsate({
+      first: false,
+      second: true,
+      third: false})
 }
 
 
@@ -133,7 +202,14 @@ function start() {
    });
 
  }
-
+ setClassPulsate({
+   first: false,
+   second: false,
+   third: true})
+   if (click) {
+   setTextInDiv(false)
+   setTimeout(() => {setTextInDiv(true)}, 4900)}
+   setClick(false)
 }
  function stop() {
    var highestTimeoutId = setTimeout(";");
@@ -142,6 +218,10 @@ function start() {
    }
    setNumber()
    setDiv("white")
+   setClassPulsate({
+     first: true,
+     second: false,
+     third: false})
  }
  function big() {
    setClassState({isSwitchOn: !classState.isSwitchOn})
@@ -196,64 +276,94 @@ function start() {
         onChange={handleChangeAmount}
       />
     </label>
-    <button onClick={loop} className="start">
-      Kliknij aby zatwierdzić czas
+    <button onClick={loop} className={classPulsate.first ? "start pulsate" : "start"}>
+      Zatwierdź czas
     </button>
-    <button onClick={start} className="start">
+    <button onClick={start} className={classPulsate.second ? "start pulsate" : "start"}>
       Start
     </button>
-    <button onClick={stop} className="start">
+    <button onClick={stop} className={classPulsate.third ? "start pulsate" : "start"}>
       Stop
     </button>
   </div>
 
-  <div style={{ backgroundColor: div }} className={classState.isSwitchOn ? "box big" : "box" } onClick={big}>{classState.isSwitchOn ? number : null} </div>
-  <div className={classState.isSwitchOn ? "numbers none" : "numbers" }>{!classState.isSwitchOn ? number : null}</div>
-    <div className={classState.isSwitchOn ? "colors none" : "colors"}>
+  <div style={{ backgroundColor: div }} className={classState.isSwitchOn ? "box big" : "box" } onClick={big}>{classState.isSwitchOn ? number : null}{!classState.isSwitchOn ? <span className={textInDiv ? "pulsateText none" : "pulsateText" }>Kliknij aby wejść w tryb pełnoekranowy!</span> : null} </div>
 
-              <label style={colors.includes("red") ? styles.red : {}}>
-                red
-                <input
-                  type="checkbox"
-                  value="red"
-                  onClick={handleWhoGive}
-                  defaultChecked={true}
-                />
-              </label>
-              <label style={colors.includes("brown") ? styles.brown : {}}>
-                brown
-                <input
-                  type="checkbox"
-                  value="brown"
-                  onClick={handleWhoGive}
-                />
-              </label>
-              <label style={colors.includes("blue") ? styles.blue : {}}>
-                blue
-                <input
-                  type="checkbox"
-                  value="blue"
-                  onClick={handleWhoGive}
-                />
-              </label>
-              <label style={colors.includes("orange") ? styles.orange : {}}>
-                orange
-                <input
-                  type="checkbox"
-                  value="orange"
-                  onClick={handleWhoGive}
-                />
-              </label>
-              <label style={colors.includes("yellow") ? styles.yellow : {}}>
-                yellow
-                <input
-                  type="checkbox"
-                  value="yellow"
-                  onClick={handleWhoGive}
-                />
-              </label>
-            </div>
+    <div className={classState.isSwitchOn ? "colors none" : "colors"}>
+    <div className="labels">
+  <div>
+    <label style={colors.includes("red") ? styles.red : {}}>
+      red
+      <input
+        type="checkbox"
+        value="red"
+        onClick={handleWhoGive}
+        defaultChecked={true}
+        onChange={handleFirstCheckbox}
+      />
+    </label>
+    <label style={colors.includes("saddlebrown") ? styles.brown : {}}>
+      brown
+      <input
+        type="checkbox"
+        value="saddlebrown"
+        onClick={handleWhoGive}
+        onChange={handleSecondCheckbox}
+      />
+    </label>
+    </div>
+    <div>
+    <label style={colors.includes("blue") ? styles.blue : {}}>
+      blue
+      <input
+        type="checkbox"
+        value="blue"
+        onClick={handleWhoGive}
+        onChange={handleThirdCheckbox}
+      />
+    </label>
+
+
+    <label style={colors.includes("orange") ? styles.orange : {}}>
+      orange
+      <input
+        type="checkbox"
+        value="orange"
+        onClick={handleWhoGive}
+        onChange={handleFourthCheckbox}
+      />
+    </label>
+    </div>
+    <div>
+    <label style={colors.includes("purple") ? styles.purple : {}}>
+      purple
+      <input
+        type="checkbox"
+        value="purple"
+        onClick={handleWhoGive}
+        onChange={handleFifthCheckbox}
+      />
+    </label>
+    <label style={colors.includes("yellow") ? styles.yellow : {}}>
+      yellow
+      <input
+        type="checkbox"
+        value="yellow"
+        onClick={handleWhoGive}
+        onChange={handleSixthCheckbox}
+      />
+    </label>
+    </div>
+  </div>
+  <span className={allCheckboxes() ? "noChecked none" : "noChecked"}>
+    Nie zaznaczono żadnego koloru!
+  </span>
+  <div className={classState.isSwitchOn ? "numbers none" : "numbers" }>{number}</div>
 </div>
+
+
+</div>
+
 
   );
 }
