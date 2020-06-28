590c5bdd4cddddf56e58f94b8a45d072d362fc39
Maciej <1994tomaszewicz@gmail.com>
2020-06-28 02:51:09 +0200

    Updates

---
+++ precache-manifest.981cc7bf051bf1909480c00cecb3de84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba6812873cb870ea0965b04a67a9a2d",
+    "url": "/beFast/index.html"
+  },
+  {
+    "revision": "bbe867ed1e6c73a34351",
+    "url": "/beFast/static/css/main.ea11e87c.chunk.css"
+  },
+  {
+    "revision": "1d8f789175ef20265252",
+    "url": "/beFast/static/js/2.6835048e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beFast/static/js/2.6835048e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe867ed1e6c73a34351",
+    "url": "/beFast/static/js/main.fc0d63c6.chunk.js"
+  },
+  {
+    "revision": "226bea8d3151fb109403",
+    "url": "/beFast/static/js/runtime-main.4eeb6150.js"
+  }
+]);
